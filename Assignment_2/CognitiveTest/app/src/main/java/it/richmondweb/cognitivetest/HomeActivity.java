08cedfc8e4e58210d64f905d2701bcb56f3aedd1
Riccardo Cannistrà <rickystream94@gmail.com>
2017-02-17 15:18:12 +0100

    Home layout implemented and communication between activities working fine

---
+++ Assignment_2/CognitiveTest/app/src/main/java/it/richmondweb/cognitivetest/HomeActivity.java
@@ -0,0 +1,55 @@
+package it.richmondweb.cognitivetest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_action_restart:
+                Log.d("Menu clicked", "restart");
+                return true;
+            case R.id.menu_action_show_results:
+                Log.d("Menu clicked", "show_results");
+                return true;
+            case R.id.menu_action_delete_data:
+                Log.d("Menu clicked", "delete_data");
+                DatabaseHelper db = DatabaseHelper.getInstance(getApplicationContext());
+                db.reset();
+                Toast.makeText(this, "All data deleted!", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_action_export_data:
+                Log.d("Menu clicked", "export_data");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void startNewTest(View view) {
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
