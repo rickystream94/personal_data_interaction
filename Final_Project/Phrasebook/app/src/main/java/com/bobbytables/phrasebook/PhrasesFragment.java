b0ea5b4437c8845f96f3a34890878572b53fdca4
Riccardo Cannistrà <rickystream94@gmail.com>
2017-03-20 22:39:03 +0100

    Implemented search in Phrasebook tab

--- Final_Project/Phrasebook/app/src/main/java/com/bobbytables/phrasebook/PhrasesFragment.java
+++ Final_Project/Phrasebook/app/src/main/java/com/bobbytables/phrasebook/PhrasesFragment.java
@@ -1,6 +1,9 @@
 package com.bobbytables.phrasebook;
 
 
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,8 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bobbytables.phrasebook.database.DatabaseHelper;
+import com.bobbytables.phrasebook.utils.SettingsManager;
 
 
 /**
@@ -18,6 +25,9 @@ import com.bobbytables.phrasebook.database.DatabaseHelper;
 public class PhrasesFragment extends Fragment {
 
     private DatabaseHelper databaseHelper;
+    private String motherLanguage;
+    private String foreignLanguage;
+    private DataRowCursorAdapter rowCursorAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,14 +42,39 @@ public class PhrasesFragment extends Fragment {
         layout = R.layout.fragment_phrases;
         View rootView = inflater.inflate(layout, container, false);
 
+        motherLanguage = SettingsManager.getInstance(getContext()).getPrefStringValue(SettingsManager
+                .KEY_MOTHER_LANGUAGE);
+        foreignLanguage = SettingsManager.getInstance(getContext()).getPrefStringValue(SettingsManager
+                .KEY_FOREIGN_LANGUAGE);
+        TextView lang1 = (TextView) rootView.findViewById(R.id.phrases_lang1);
+        TextView lang2 = (TextView) rootView.findViewById(R.id.phrases_lang2);
+        lang1.setText(motherLanguage);
+        lang2.setText(foreignLanguage);
+
+        // Get the SearchView and set it properly
+        SearchView searchView = (SearchView) rootView.findViewById(R.id.search_phrase);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchPhrase(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                searchPhrase(query);
+                return false;
+            }
+        });
+        searchView.setQueryHint(getString(R.string.search_hint));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
         //Creating cursor adapter to attach to list view
         Cursor dataCursor = getAllPhrases();
-        DataRowCursorAdapter rowCursorAdapter = new DataRowCursorAdapter(getContext(), dataCursor);
+        rowCursorAdapter = new DataRowCursorAdapter(getContext(), dataCursor);
         ListView dataListView = (ListView) rootView.findViewById(R.id.dataListView);
         dataListView.setAdapter(rowCursorAdapter);
 
-        // Switch to new cursor and update contents of ListView
-        //rowCursorAdapter.changeCursor(newCursor);
         return rootView;
     }
 
@@ -47,4 +82,10 @@ public class PhrasesFragment extends Fragment {
         return databaseHelper.getDataFromTable(DatabaseHelper.TABLE_PHRASES);
     }
 
+    public void searchPhrase(String query) {
+        Cursor cursor = databaseHelper.searchPhrase(query);
+        // Switch to new cursor and update contents of ListView
+        rowCursorAdapter.changeCursor(cursor);
+        rowCursorAdapter.notifyDataSetChanged();
+    }
 }
