daa90a2d61b759ce1e233bda56a8125cebf2df6a
Riccardo Cannistrà <rickystream94@gmail.com>
2017-03-22 02:29:32 +0100

    Implemented basic data visualization of challenges with pie chart.

--- Final_Project/Phrasebook/app/src/main/java/com/bobbytables/phrasebook/ProgressFragment.java
+++ Final_Project/Phrasebook/app/src/main/java/com/bobbytables/phrasebook/ProgressFragment.java
@@ -1,6 +1,7 @@
 package com.bobbytables.phrasebook;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bobbytables.phrasebook.database.DatabaseHelper;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -22,11 +30,35 @@ public class ProgressFragment extends Fragment {
                              Bundle savedInstanceState) {
         databaseHelper = DatabaseHelper.getInstance(getContext());
         int layout;
-        if (databaseHelper.isDatabaseEmpty())
-            layout = R.layout.empty_database;
-        else layout = R.layout.fragment_progress;
         // Inflate the layout for this fragment
-        return inflater.inflate(layout, container, false);
+        if (databaseHelper.isDatabaseEmpty()) {
+            layout = R.layout.empty_database;
+            return inflater.inflate(layout, container, false);
+        }
+        layout = R.layout.fragment_progress;
+        View rootView = inflater.inflate(layout, container, false);
+
+        PieChart pieChart = (PieChart) rootView.findViewById(R.id.challengePieChart);
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(75, "Won"));
+        entries.add(new PieEntry(25, "Lost"));
+        PieDataSet dataSet = new PieDataSet(entries, "Challenges");
+        dataSet.setValueTextSize(18f);
+        //dataSet.setColor(...);
+        //dataSet.setValueTextColor(...); // styling, ...
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(0, Color.GREEN);
+        colors.add(1, Color.RED);
+        dataSet.setColors(colors);
+        dataSet.setValueTextColor(Color.WHITE);
+        PieData pieData = new PieData(dataSet);
+        pieChart.setData(pieData);
+        pieChart.setCenterText("Your Challenges:\n20 Won\n10 Lost");
+        pieChart.setCenterTextSize(24f);
+        pieChart.setEntryLabelTextSize(18f);
+        pieChart.setHoleRadius(70);
+        pieChart.invalidate(); //refresh
+        return rootView;
     }
 
 }
