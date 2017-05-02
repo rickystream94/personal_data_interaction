3c287e7ec4c04e71a77cd5da4cc363e38c33e5f2
Riccardo Cannistrà <rickystream94@gmail.com>
2017-05-02 14:49:22 +0200

    Released revision 7, experiment conducted in 3+3 days

--- Final_Project/Phrasebook/app/src/main/java/com/bobbytables/phrasebook/MainActivity.java
+++ Final_Project/Phrasebook/app/src/main/java/com/bobbytables/phrasebook/MainActivity.java
@@ -114,7 +114,7 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
                     .KEY_SWITCHED_VERSION);
             boolean hasPerformedLastUpload = settingsManager.getPrefBoolValue(SettingsManager
                     .KEY_FINAL_UPLOAD_PERFORMED);
-            if (daysDiff >= 5 && !hasAlreadySwitchedVersion) {
+            if (daysDiff >= 3 && !hasAlreadySwitchedVersion) {
                 try {
                     //Try to upload data before switching to new version
                     executeUpload();
@@ -135,7 +135,7 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
                 }
             }
             //perform latest automatic data upload
-            if (daysDiff >= 10 && hasAlreadySwitchedVersion && !hasPerformedLastUpload) {
+            if (daysDiff >= 6 && hasAlreadySwitchedVersion && !hasPerformedLastUpload) {
                 try {
                     //Try to upload data before switching to new version
                     executeUpload();
